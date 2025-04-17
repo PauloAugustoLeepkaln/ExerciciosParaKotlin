5dbb32c97f547506c6219a626af211ba515f8ed3
Gabrielhilll <eversonhillesheim@hotmail.com>
2025-04-16 21:03:00 -0300

    socorro

--- src/Aula_dia_16_04/Pilha.kt
+++ src/Aula_dia_16_04/Pilha.kt
@@ -1,7 +1,12 @@
 package Aula_dia_16_04
 
+import java.util.*
+
 class Pilha {
     fun main(){
+        val pilha = LinkedList<String>()
 
+        //Prateleira de produtos
+        pilha.push()
     }
 }
